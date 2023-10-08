cb74de0795bb556f336538f428942baa5e76d6ff
Roman <kurpelroma@gmail.com>
2023-10-08 17:32:23 +0300

    created modal

--- js/script.js
+++ js/script.js
@@ -4,17 +4,17 @@
 // const menu = document?.querySelector('.menu__body');
 // const body = document?.querySelector('body');
 
-// const cases = document?.querySelectorAll('.swiper-slide');
-// const modal = document?.querySelector('.modal');
-// const btn = document?.querySelector('.closeModalBtn');
+const cases = document?.querySelectorAll('.swiper-slide');
+const modal = document?.querySelector('.modal');
+const btn = document?.querySelector('.closeModalBtn');
 
-// let isModalOpen = false
+let isModalOpen = false
 
-// window.addEventListener('click', (event) => {
-//   if (isModalOpen && event.target.classList.contains('modal') | event.target.classList.contains('close')) {
-//     closeModal()
-//   }
-// });
+window.addEventListener('click', (event) => {
+  if (isModalOpen && event.target.classList.contains('modal') | event.target.classList.contains('close')) {
+    closeModal()
+  }
+});
 
 new Swiper('.swiper', {
   slidesPerView: 1,
@@ -37,126 +37,101 @@ new Swiper('.swiper', {
   }
 });
 
-// document.addEventListener('click', (e) => {
-//   if (e.target.classList.contains('anchor')) {
-//     e.preventDefault()
-//     anchorClick(e.target)
-//   }
-//   if (e.target.classList.contains('btn__anchor')) {
-//     e.preventDefault()
-//     anchorClick(e.target.parentNode)
-//   }
-//   if (e.target.classList.contains('header__burger')) {
-//     console.log(e.target);
-//     e.preventDefault()
-//     toggleMenu()
-//   }
-// })
-// function anchorClick(e) {
-//   if (menu.classList.contains('active')) {
-//     toggleMenu()
-//   }
-//   const blockId = e.getAttribute('href')
-//   document.querySelector('' + blockId).scrollIntoView({
-//     behavior: "smooth",
-//     block: "start",
-//     inline: "nearest"
-//   })
-// }
-// function toggleMenu() {
-//   menu.classList.toggle('active');
-//   burger.classList.toggle('active');
-//   burger.classList.contains('active') ? document.body.classList.add('scroll--block') : document.body.classList.remove('scroll--block')
-// }
-
-
-// // Отримуємо всі зображення у слайдері
-// const images = document.querySelectorAll('.swiper__slide-img');
-
-// // Встановлюємо бажану висоту для зображень
-// const desiredHeight = 300; // Ваша бажана висота
-
-// // Змінюємо розмір зображень
-// images.forEach(image => {
-//   const imgWidth = image.naturalWidth;
-//   const imgHeight = image.naturalHeight;
-
-//   const newWidth = (desiredHeight / imgHeight) * imgWidth;
-
-//   image.style.width = newWidth + 'px';
-//   image.style.height = desiredHeight + 'px';
-// });
 
 
 
-// /*
+// Отримуємо всі зображення у слайдері
+const images = document.querySelectorAll('.swiper__slide-img');
 
-// -Модалка
+// Встановлюємо бажану висоту для зображень
+const desiredHeight = 174; // Ваша бажана висота
 
-// */
+// Змінюємо розмір зображень
+images.forEach(image => {
+  const imgWidth = image.naturalWidth;
+  const imgHeight = image.naturalHeight;
 
-// const openModal = (item) => {
-//   const name = item.querySelector('.swiper__content-name').textContent
-//   const text = item.querySelector('.swiper__content-text').textContent
-//   const expert = item.querySelector('.swiper__content-expert').textContent
-//   const imgUrl = item.querySelector('.swiper__content-img').src;
+  const newWidth = (desiredHeight / imgHeight) * imgWidth;
 
-//   document.body.classList.add('body-block');
-
-//   modal.style.display = 'block';
-//   isModalOpen = true
-//   modal.classList.add('fadeIn'); // Додаємо клас анімації
-//   // Додати вміст до модального вікна
-//   const modalContent = `
-//     <div class="modal-content">
-//       <span id="closeModalBtn" class="close">&times;</span>
-//       <div class="modal__content-top">
-//         <img class="swiper__content-img" src="${imgUrl}" alt="image">
-//         <div class="modal__content-top__text">
-//             <span class="swiper__content-name">${name}</span> 
-//             <span class="swiper__content-expert">${expert}</span> 
-//         </div>                
-//       </div>
-//       <p class="modal-content-text">${text}</p>
-//     </div>
-//   `;
-//   modal.innerHTML = modalContent;
-
-// }
-
-// const closeModal = () => {
-//   modal.style.display = 'none';
-//   modal.classList.remove('fadeIn'); // Додаємо клас анімації
-//   document.body.classList.remove('body-block');
-// }
+  image.style.width = newWidth + 'px';
+  image.style.height = desiredHeight + 'px';
+});
 
 
-// // ... інший код ...
 
 
-// for (let item = 0; item < cases.length; item++) {
-//   const slideText = cases[item].querySelector('.swiper__content-text')
-//   const slideBtn = cases[item].querySelector('.swiper__content-read')
-//   const heightText = slideText.offsetHeight;
-//   if (heightText > 236) {
-//     slideText.classList.add('gradient-overlay');
-//     cases[item].style.cursor = 'pointer';
+// Модалка
 
-//     // Зберігаємо посилання на елемент cases[item]
-//     const currentCase = cases[item];
-//     cases[item].addEventListener('click', (e) => {
-//       e.stopPropagation();
-//       if (currentCase.contains(e.target)) {
-//         activeSlide = e.target
-//         openModal(cases[item]);
-//       }
-//     });
 
-//   } else {
-//     slideBtn.classList.add('no-btn');
-//     slideText.style.paddingBottom = '25.9px';
-//   }
-// }
+const openModal = (item) => {
+  const name = item.querySelector('.swiper__content-name').textContent
+  const text = item.querySelector('.swiper__content-text').textContent
+  const textLocation = item.querySelector('.swiper__content-location').textContent
+  const textInstagram = item.querySelector('.swiper__content-instagram').textContent
+  const imgUrl = item.querySelector('.swiper__content-img').src;
+  
+  document.body.classList.add('body-block');
+
+  modal.style.display = 'block';
+  isModalOpen = true
+  modal.classList.add('fadeIn'); // Додаємо клас анімації
+  // Додати вміст до модального вікна
+  const modalContent = `
+    <div class="modal-content">
+      <span id="closeModalBtn" class="close">&times;</span>
+      <div class="modal__content-top">
+        <img class="swiper__content-img" src="${imgUrl}" alt="image">
+        
+        <div class="modal__content-top__text">
+            <span class="swiper__content-name">${name}</span> 
+            <span class="swiper__content-location">
+               ${textLocation}
+             </span>
+             <span class="swiper__content-instagram">
+               ${textInstagram}
+             </span>
+        </div>                
+      </div>
+      <p class="modal-content-text">${text}</p>
+    </div>
+  `;
+  modal.innerHTML = modalContent;
+
+}
+
+const closeModal = () => {
+  modal.style.display = 'none';
+  modal.classList.remove('fadeIn'); // Додаємо клас анімації
+  document.body.classList.remove('body-block');
+}
+
+
+// ... інший код ...
+
+
+for (let item = 0; item < cases.length; item++) {
+  const slideText = cases[item].querySelector('.swiper__content-text')
+  const slideBtn = cases[item].querySelector('.swiper__content-read')
+  const heightText = slideText.offsetHeight;
+  if (heightText > 236) {
+    slideText.classList.add('gradient-overlay');
+    cases[item].style.cursor = 'pointer';
+
+    // Зберігаємо посилання на елемент cases[item]
+    const currentCase = cases[item];
+    cases[item].addEventListener('click', (e) => {
+      e.stopPropagation();
+      if (currentCase.contains(e.target)) {
+        activeSlide = e.target
+        openModal(cases[item]);
+      }
+    });
+
+  } else {
+    slideBtn.classList.add('no-btn');
+    slideText.style.paddingBottom = '25.9px';
+  }
+}
 
 
 const acc = document?.querySelectorAll('.accordeon');
@@ -183,8 +158,6 @@ const loop = (archive) => {
     })
   }
 }
-
-
 
 const scrollToAnchor = (blockId) => {
   document.querySelector('' + blockId).scrollIntoView({
